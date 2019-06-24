a0d3608d13771ff0edcc69b776562d01e95df218
Varun <uberkumar@gmail.com>
2019-06-24 22:00:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,40 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+ game_hash = {
+  home : 
+    team_name : "Brooklyn Nets"
+    colors : ["Black", "White"]
+    players : [
+      {player_name : 'Alan Anderson'}, 
+      {number : 0 }, 
+      {shoe : 16 }, 
+      {points : 22 }, 
+      {rebounds : 12 }, 
+      {assists : 12 }, 
+      {steals : 3 }, 
+      {blocks : 1 }, 
+      {slam_dunks: 1 }
+      
+      {player_name : 'Alan Anderson'}, 
+      {number : 0 }, 
+      {shoe : 16 }, 
+      {points : 22 }, 
+      {rebounds : 12 }, 
+      {assists : 12 }, 
+      {steals : 3 }, 
+      {blocks : 1 }, 
+      {slam_dunks: 1}
+    ]
+  away :
+    :team_name =>
+    :colors =>
+    :players : [
+      {player_name : 'Alan Anderson'}, 
+      {number : }, 
+      {shoe : }, 
+      {points : }, 
+      {rebounds : }, 
+      {assists : }, 
+      {steals : }, 
+      {blocks : }, 
+      {slam_dunks: }
+    ]
+}
